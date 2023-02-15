9c7c6e7ccacc61e61358fc1979a850812c5d8e58
Mark <markbronshteyn@mail.ru>
2023-02-15 18:03:40 +0800

    Создал реакт-апп

---
+++ svoyak/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Здесь когда-то будет свояк, но это уже совсем другая история
+        </p>
+        <a
+          className="App-link"
+          href="https://vk.com/id47066215"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Тык
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
